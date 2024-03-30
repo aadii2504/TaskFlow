2e0e57f16fdb3b18828ca513231fbc1d70098994
Aadii2504 <adityadhaygude25@gmail.com>
2024-03-30 20:06:43 +0530

    Final commit with changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState , useEffect} from 'react'
 import Navbar from './components/Navbar'
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteSweep } from "react-icons/md";
 import { v4 as uuidv4 } from 'uuid';
+
 
 
 function App() {
@@ -81,27 +84,34 @@ const toggleFinished = (e) => {
   return (
     <>
     <Navbar/>
-      <div className="container mx-auto rounded-lg my-5 bg-violet-100 p-5 min-h-[80vh]">
-        <div className="addTodo my-5">
-          <h2 className='text-lg font-bold'>Add Todo</h2>
-          <input onChange={handleChange} value={todo} type="text" className='w-1/2 rounded-lg'/>
-          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-500 hover:bg-violet-800 disabled:bg-violet-900 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md mx-6'>Add</button>
+ 
+      <div className=" shadow-2xl  border border-opacity-95 border-white mx-3 md:container md:mx-auto rounded-xl my-5 bg-violet-100 p-5 min-h-[80vh] md:w-1/2">
+        <h1 className='font-bold text-center text-3xl'>TaskFlow -  Manage your tasks at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className='text-2xl font-bold'>Add Todo</h2>
+          <div className="flex">
+
+          <input onChange={handleChange} value={todo} type="text" className='rounded-lg w-full px-5 py-1'/>
+          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-500 hover:bg-violet-800 mx-2 disabled:bg-violet-900 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md cursor-pointer'>Add</button>
+          </div>
         </div>
-        <input onChange={toggleFinished} type="checkbox" checked={showFinished} /> Show Finished Tasks
-          <h2 className='text-lg font-bold'>Your Todos</h2>
+        <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
+        <label className='mx-2' htmlFor="show">Show Finished Tasks</label>
+        <div className='h-[1px] bg-black  opacity-15'></div>
+          <h2 className='text-2xl font-bold my-4'>Your Todos</h2>
         <div className="todos">
           {todos.length === 0 && <div className='m-5 font-semibold text-2xl'> No Todos To Display :(</div>}
           {todos.map(item=>{
 
          
-          return <div key={item.id} className="todo flex my-3 w-1/4 justify-between ">
+          return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex my-3  justify-between ">
             <div className='flex gap-5'>
             <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
                 <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
             </div>
                 <div className="buttons flex h-full">
-                  <button onClick={(e)=>handleEdit(e,item.id)} className='bg-violet-500 hover:bg-violet-800 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md mx-1'>Edit</button>
-                  <button onClick={(e)=>{handleDelete(e ,item.id)}} className='bg-violet-500 hover:bg-violet-800 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md mx-1'>Delete</button>
+                  <button onClick={(e)=>handleEdit(e,item.id)} className='bg-violet-500 hover:bg-violet-800 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md mx-1'><CiEdit /></button>
+                  <button onClick={(e)=>{handleDelete(e ,item.id)}} className='bg-violet-500 hover:bg-violet-800 hover:text-opacity-45 p-5 py-1 text-sm font-bold text-black rounded-md mx-1'><MdDeleteSweep /></button>
                 </div>
           </div>
            })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +19,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [addVariablesForColors],
+};
